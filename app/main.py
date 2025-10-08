13b5f1accb4a8f055742466688896e8db6fa74f5
jressym <jressym@gmail.com>
2025-10-08 12:13:43 +0700

    Update terbaru admin_film + studio
    Jadi nambah API studio

--- app/main.py
+++ app/main.py
@@ -1,20 +1,25 @@
+# main.py
+# ==========================================
+# Entry point utama aplikasi FastAPI
+# ==========================================
+
 from fastapi import FastAPI
-from app.routers import admin_film
+from app.routers import admin_film  # folder app/routers/ ada file __init__.py
 
 # Inisialisasi aplikasi FastAPI
 app = FastAPI(
     title="Movie Booking System",
-    description="Sistem pemesanan tiket bioskop sederhana (versi Person 1 - Manajemen Film)",
-    version="1.0.0"
+    description="Sistem pemesanan tiket bioskop sederhana",
+    version="1.1.0"
 )
 
-# Tambahkan router untuk fitur Admin Film
-app.include_router(admin_film.router, tags=["Admin - Film"])
+# Registrasi router (Admin Film & Studio)
+app.include_router(admin_film.router, tags=["Admin - Film & Studio"])
 
 # Endpoint root (opsional, hanya untuk cek server jalan)
 @app.get("/")
 def home():
     return {
         "message": "Selamat datang di Movie Booking System!",
-        "info": "Gunakan endpoint /docs untuk melihat dokumentasi API."
+        "info": "Gunakan endpoint /docs untuk mencoba semua fitur API."
     }
--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from typing import Optional, List, Dict
 
 
 # ===============================
-# 1️⃣ MODEL UNTUK ADMIN FILM
+# MODEL UNTUK ADMIN FILM
 # ===============================
 class Movie(BaseModel):
     """
@@ -19,5 +19,17 @@ class Movie(BaseModel):
     duration: str           # contoh: "3h 20m"
     genre: str 
     sutradara: str
-    rating: Optional[float] = None  # opsional (bisa diisi nanti)
-    price: int              # contoh: 50000
+    rating_usia: str  # opsional (bisa diisi nanti)
+    price: str              # contoh: 50000
+
+# ===============================
+# MODEL UNTUK STUDIO
+# ===============================
+class Studio(BaseModel):
+    """
+    Representasi data studio yang menayangkan film tertentu.
+    Terhubung dengan data Movie melalui id_movie.
+    """
+    id_studio: str          # contoh: "st1"
+    id_movie: str           # contoh: "mov1"
+    title: str
